6ed58df7f6ed756c26673abffc4ee3282ee6fadf
junpyoseo <jpseo99@gmail.com>
2023-12-28 13:03:42 +0900

    add

--- docker.sh
+++
@@ -1,3 +0,0 @@
-docker run -it --gpus all --ipc host -v /home/jpseo99/Pointerformer:/home/Pointerformer 9ef37be4ff59
-# nvidia/cuda   12.3.1-devel-ubuntu20.04   9ef37be4ff59   2 weeks ago   7.03GB
-
